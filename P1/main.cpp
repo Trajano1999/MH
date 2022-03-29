6947170d45c0e117f677e490a58191bfb1dada4e
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2022-03-29 21:09:55 +0200

    Primera subida

---
+++ P1/main.cpp
@@ -0,0 +1,12 @@
+//-------------------------------------------------------------------------------------------------
+//  Universidad de Granada
+//  Escuela Superior de Inegeniería Informática y Telecomunicaciones 
+//  Curso 2021/2022 
+//  
+//  Metaheurísticas
+//  Práctica 1:
+//  Técnicas de Búsqueda Local y Algoritmos Greedy para el Problema de la Mínima Dispersión Diferencial
+//
+//  Juan Manuel Mateos Pérez
+//-------------------------------------------------------------------------------------------------
+
