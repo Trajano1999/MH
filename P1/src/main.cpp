a092fd5d18b672f8c0d21f716c88d9c1088d8ece
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2022-04-10 18:40:45 +0200

    modificaciones P1

--- P1/src/main.cpp
+++ P1/src/main.cpp
@@ -55,16 +55,24 @@ int main(int narg, char * arg[]){
              tiempo_antes_BL,
              tiempo_despues_BL;
     double tiempo_final_greedy = 0.0,
-           tiempo_final_BL = 0.0;
+           tiempo_final_BL = 0.0,
+           desviacion_final = 0.0;
     const char * dir_fichero = arg[1];
     string nombre_fichero;
     
-    // mensajes por pantalla
+    // vector con las soluciones ideales proporcionadas por el profesor
+    vector<double> soluciones_ideales = { 0, 0, 0, 0, 0, 12.7179599999997, 14.0987499999997, 16.7611899999998, 17.0692099999999, 23.2652299999999, 1.92610000000002, 2.12104000000011,       \
+        2.36230999999998, 1.6631999999999, 2.85312999999996, 42.7457799999991, 48.1076099999996, 43.1960899999997, 46.4124499999994, 47.7151099999996, 13.8320199999999, 13.6643400000003,   \
+        15.3453799999998, 8.64063999999939, 17.2005099999999, 168.729590000001, 127.09726, 106.379189999999, 137.453159999999, 127.479740000001, 11.7451399999999, 18.7889299999997,         \
+        18.5315999999998, 19.4883300000001, 18.1124199999998, 155.434770000002, 198.894619999999, 187.96703, 168.590200000001, 178.193740000003, 23.3460800000003, 26.7894999999996,         \
+        26.7544699999996, 25.9355900000005, 27.7730099999994, 227.749309999998, 228.6029, 226.745339999999, 226.409610000003, 248.856619999998 };
+    
+    // mensajes por pantalla jjj
     string peticion_semilla = "\nIntroduzca la semilla : ",
            solucion_greedy  = "\nSolución por Greedy         : ",
            solucion_BL      = "\nSolución por Búsqueda Local : ";
 
-    // guardamos los datos en un fichero
+    // guardamos los datos en un fichero externo
     ofstream fichero(DIR_FICHERO_SALIDA);
     if(!fichero.is_open())
         cerr << "Error al abrir " << DIR_FICHERO_SALIDA << endl;
@@ -93,24 +101,20 @@ int main(int narg, char * arg[]){
             tiempo_final_BL += double(tiempo_despues_BL-tiempo_antes_BL) / CLOCKS_PER_SEC;
         }
         
+        // calculamos los tiempos
         tiempo_final_greedy = tiempo_final_greedy / EJECUCIONES_POR_PROBLEMA;
         tiempo_final_BL = tiempo_final_BL / EJECUCIONES_POR_PROBLEMA;
 
-        fichero << arg[i] << " " << tiempo_final_greedy << " " << tiempo_final_BL << endl;
+        // eliminamos la ruta de los archivos
+        nombre_fichero = arg[i];
+        cout << "\n" << nombre_fichero;
+        nombre_fichero.erase(nombre_fichero.begin(), nombre_fichero.begin()+59);
+        
+        fichero << nombre_fichero << " " << tiempo_final_greedy << " " << tiempo_final_BL << endl;
 
         tiempo_final_greedy = 0;
         tiempo_final_BL = 0;
     }
-    
-    /*random_semilla = 1; jjj
-
-    // solución por Greedy
-    cout << solucion_greedy;
-    mostrarVector(problema.solucionGreedy());
-
-    // solución por Búsqueda Local
-    cout << solucion_BL;
-    mostrarVector(problema.solucionBusquedaLocal());*/
 
     cout << endl;
 }
