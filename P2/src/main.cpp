784c493ff11858a69043044ba14c2dde4b0aa04e
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2022-05-19 15:18:04 +0200

    modificaciones

--- P2/src/main.cpp
+++ P2/src/main.cpp
@@ -18,8 +18,20 @@ using namespace std;
 // CONSTANTES GLOBALES
 //-------------------------------------------------------------------------------------------------
 
-const unsigned EJECUCIONES_POR_PROBLEMA = 5;
-const string DIR_FICHERO_SALIDA = "/home/juanma/Escritorio/Uni/Segundo_Cuatri/MH/P2/datos2.txt";
+const unsigned RAMDOM_SEMILLA = 0,
+               EJECUCIONES_POR_PROBLEMA = 5;
+
+// jjj               
+const string DIR_FICHERO_SALIDA = "/home/juanma/Escritorio/Uni/Segundo_Cuatri/MH/P2/datos.txt";
+
+// vector con las soluciones ideales proporcionadas por el profesor
+const vector<double> SOLUCIONES_PROFESOR = {
+    0, 0, 0, 0, 0, 12.7179599999997, 14.0987499999997, 16.7611899999998, 17.0692099999999, 23.2652299999999, 1.92610000000002, 2.12104000000011, 2.36230999999998,                 \
+    1.6631999999999, 2.85312999999996, 42.7457799999991, 48.1076099999996, 43.1960899999997, 46.4124499999994, 47.7151099999996, 13.8320199999999, 13.6643400000003,               \
+    15.3453799999998, 8.64063999999939, 17.2005099999999, 168.729590000001, 127.09726, 106.379189999999, 137.453159999999, 127.479740000001, 11.7451399999999,                     \
+    18.7889299999997, 18.5315999999998, 19.4883300000001, 18.1124199999998, 155.434770000002, 198.894619999999, 187.96703, 168.590200000001, 178.193740000003,                     \
+    23.3460800000003, 26.7894999999996, 26.7544699999996, 25.9355900000005, 27.7730099999994, 227.749309999998, 228.6029, 226.745339999999, 226.409610000003, 248.856619999998     \
+};
 
 //-------------------------------------------------------------------------------------------------
 // FUNCIONES
@@ -64,58 +76,6 @@ void mostrarVector(const vector<int> & v)
 
 /*int main(int narg, char * arg[])
 {
-    unsigned random_semilla = 0;
-    const char * dir_fichero = arg[1];
-    vector<string> resultadosP1, resultadosP2;
-    Problema problema(random_semilla, dir_fichero);
-
-    // vector con las soluciones ideales proporcionadas por el profesor
-    vector<double> soluciones_ideales = { 0, 0, 0, 0, 0, 12.7179599999997, 14.0987499999997, 16.7611899999998, 17.0692099999999, 23.2652299999999, 1.92610000000002, 2.12104000000011,       \
-        2.36230999999998, 1.6631999999999, 2.85312999999996, 42.7457799999991, 48.1076099999996, 43.1960899999997, 46.4124499999994, 47.7151099999996, 13.8320199999999, 13.6643400000003,   \
-        15.3453799999998, 8.64063999999939, 17.2005099999999, 168.729590000001, 127.09726, 106.379189999999, 137.453159999999, 127.479740000001, 11.7451399999999, 18.7889299999997,         \
-        18.5315999999998, 19.4883300000001, 18.1124199999998, 155.434770000002, 198.894619999999, 187.96703, 168.590200000001, 178.193740000003, 23.3460800000003, 26.7894999999996,         \
-        26.7544699999996, 25.9355900000005, 27.7730099999994, 227.749309999998, 228.6029, 226.745339999999, 226.409610000003, 248.856619999998 };
-
-    // variables para tomar los datos
-    double tiempo_antes,
-           tiempo_despues,
-
-           tiempo_greedy,
-           media_tiempo_greedy = 0,
-           valor_greedy,
-           desv_tipica_greedy,
-           media_desv_greedy = 0,
-
-           tiempo_BL,
-           media_tiempo_BL = 0,
-           valor_BL,
-           desv_tipica_BL,
-           media_desv_BL = 0,
-
-           tiempo_AGGU,
-           media_tiempo_AGGU = 0,
-           valor_AGGU,
-           desv_tipica_AGGU,
-           media_desv_AGGU = 0,
-
-           tiempo_AGGP,
-           media_tiempo_AGGP = 0,
-           valor_AGGP,
-           desv_tipica_AGGP,
-           media_desv_AGGP = 0,
-
-           tiempo_AGEU,
-           media_tiempo_AGEU = 0,
-           valor_AGEU,
-           desv_tipica_AGEU,
-           media_desv_AGEU = 0,
-
-           tiempo_AGEP,
-           media_tiempo_AGEP = 0,
-           valor_AGEP,
-           desv_tipica_AGEP,
-           media_desv_AGEP = 0;
-
     // guardamos los datos en un fichero externo
     ofstream fichero(DIR_FICHERO_SALIDA);
     if(!fichero.is_open())
@@ -124,44 +84,12 @@ void mostrarVector(const vector<int> & v)
     // tomamos los datos
     for(int i=1; i<narg; ++i)
     {
-        cout << "\nComienza la iteración : " << i << " de 50 " << endl;
         
         problema.setMatriz(arg[i]);
         string nombre_fichero = arg[i];
 
         // eliminamos la ruta de los archivos
         nombre_fichero.erase(nombre_fichero.begin(), nombre_fichero.begin()+59);
-
-        // calculamos los tiempos
-        tiempo_antes = clock();
-        vector<int> v_Greedy = problema.solucionGreedy();
-        tiempo_despues = clock();
-        tiempo_greedy = tiempo_despues - tiempo_antes;
-
-        tiempo_antes = clock();
-        vector<int> v_BL = problema.solucionBusquedaLocal();
-        tiempo_despues = clock();
-        tiempo_BL = tiempo_despues - tiempo_antes;
-
-        tiempo_antes = clock();
-        vector<int> v_AGGU = problema.solucionAGGUniforme();
-        tiempo_despues = clock();
-        tiempo_AGGU = tiempo_despues - tiempo_antes;
-
-        tiempo_antes = clock();
-        vector<int> v_AGGP = problema.solucionAGGPosicion();
-        tiempo_despues = clock();
-        tiempo_AGGP = tiempo_despues - tiempo_antes;
-
-        tiempo_antes = clock();
-        vector<int> v_AGEU = problema.solucionAGEUniforme();
-        tiempo_despues = clock();
-        tiempo_AGEU = tiempo_despues - tiempo_antes;
-
-        tiempo_antes = clock();
-        vector<int> v_AGEP = problema.solucionAGEPosicion();
-        tiempo_despues = clock();
-        tiempo_AGEP = tiempo_despues - tiempo_antes;
         
         // calculamos las desv. típicas
         valor_greedy = problema.dispersion(v_Greedy);
@@ -227,10 +155,7 @@ void mostrarVector(const vector<int> & v)
 
 int main(int narg, char * arg[])
 {
-    unsigned random_semilla = 0;
-    const char * dir_fichero = arg[1];
-    vector<string> resultadosP1, resultadosP2;
-    Problema problema(random_semilla, dir_fichero);
+    Problema problema(RAMDOM_SEMILLA, arg[1]);
 
     double tiempo_antes,
            tiempo_despues,
@@ -241,12 +166,79 @@ int main(int narg, char * arg[])
            //desv_tipica_greedy,
            //media_desv_greedy = 0;
 
-    tiempo_antes = clock();
-    problema.solucionGreedy();
-    tiempo_despues = clock();
-    tiempo_greedy = tiempo_despues - tiempo_antes;
+           /*tiempo_BL,
+           media_tiempo_BL = 0,
+           valor_BL,
+           desv_tipica_BL,
+           media_desv_BL = 0,
 
-    cout << "Tiempo Greedy : " << tiempo_greedy << endl;
+           tiempo_AGGU,
+           media_tiempo_AGGU = 0,
+           valor_AGGU,
+           desv_tipica_AGGU,
+           media_desv_AGGU = 0,
 
+           tiempo_AGGP,
+           media_tiempo_AGGP = 0,
+           valor_AGGP,
+           desv_tipica_AGGP,
+           media_desv_AGGP = 0,
+
+           tiempo_AGEU,
+           media_tiempo_AGEU = 0,
+           valor_AGEU,
+           desv_tipica_AGEU,
+           media_desv_AGEU = 0,
+
+           tiempo_AGEP,
+           media_tiempo_AGEP = 0,
+           valor_AGEP,
+           desv_tipica_AGEP,
+           media_desv_AGEP = 0;*/
+
+    // recorremos todos los archivos
+    for(int i=1; i<narg; ++i)
+    {
+        problema.setMatriz(arg[i]);
+        string nombre_fichero = arg[i];
+
+        // eliminamos la ruta de los archivos
+        nombre_fichero.erase(nombre_fichero.begin(), nombre_fichero.begin()+36);
+
+        // calculamos los tiempos
+        tiempo_antes = clock();
+        vector<int> v_Greedy = problema.solucionGreedy();
+        tiempo_despues = clock();
+        tiempo_greedy = tiempo_despues - tiempo_antes;
+
+        /*tiempo_antes = clock();
+        vector<int> v_BL = problema.solucionBusquedaLocal();
+        tiempo_despues = clock();
+        tiempo_BL = tiempo_despues - tiempo_antes;
+
+        tiempo_antes = clock();
+        vector<int> v_AGGU = problema.solucionAGGUniforme();
+        tiempo_despues = clock();
+        tiempo_AGGU = tiempo_despues - tiempo_antes;
+
+        tiempo_antes = clock();
+        vector<int> v_AGGP = problema.solucionAGGPosicion();
+        tiempo_despues = clock();
+        tiempo_AGGP = tiempo_despues - tiempo_antes;
+
+        tiempo_antes = clock();
+        vector<int> v_AGEU = problema.solucionAGEUniforme();
+        tiempo_despues = clock();
+        tiempo_AGEU = tiempo_despues - tiempo_antes;
+
+        tiempo_antes = clock();
+        vector<int> v_AGEP = problema.solucionAGEPosicion();
+        tiempo_despues = clock();
+        tiempo_AGEP = tiempo_despues - tiempo_antes;*/
+
+        cout << "Tiempo Greedy : " << tiempo_greedy << endl;
+    }
+
+    cout << endl;
     return 0;
 }
